6670b6998c6543e2946a95df96a8aff8b58560c9
Selcuk <1202320@student.roc-nijmegen.nl>
2023-04-28 13:50:17 +0100

    second-commit

--- website/navbar-not-logged-in.php
+++ website/navbar-not-logged-in.php
@@ -1,3 +1,12 @@
 <div class="nav-container">
+    <video class="nav-logo" autoplay plays-inline muted>
+    <source src="media/logo.mp4" type="video/mp4">
+    </video>
+    <h1 class="nav-text" >World Wide Booking</h1>
+    <form class="searchbar"  method="post">
+    <input type="text" class="search" name="zoekveld" placeholder="Search">
     
+    </form>
+    <a class="nav-login-button" href="login.php">Login</a>
+    <a class="nav-signup-button" href="sign-up.php">Sign Up</a>
 </div>
