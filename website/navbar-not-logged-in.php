d0872cc8d47c8967c21e64f730667690bd8959d5
Selcuk <1202320@student.roc-nijmegen.nl>
2023-04-28 14:24:02 +0100

    navbar

--- website/navbar-not-logged-in.php
+++ website/navbar-not-logged-in.php
@@ -1,11 +1,14 @@
 <div class="nav-container">
-    <video class="nav-logo" autoplay plays-inline muted>
+    <video class="nav-logo" autoplay loop plays-inline muted>
     <source src="media/logo.mp4" type="video/mp4">
     </video>
     <h1 class="nav-text" >World Wide Booking</h1>
     <form class="searchbar"  method="post">
-    <input type="text" class="search" name="zoekveld" placeholder="Search">
-    
+    <input type="text" class="search" name="zoekveld" placeholder="Search Your Destination">
+    <svg class="tet" width="24" height="20" viewBox="0 0 24 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M17.5981 12.8978H16.486L16.0918 12.5812C17.7811 10.9398 18.654 8.7004 18.1753 6.3203C17.5137 3.06086 14.2476 0.457994 10.3058 0.0593566C4.35092 -0.550324 -0.660776 3.62364 0.0712697 8.58315C0.549915 11.866 3.67519 14.5862 7.58881 15.1372C10.4466 15.5359 13.1355 14.8089 15.1064 13.402L15.4865 13.7303V14.6565L21.4695 19.6395C22.0467 20.1202 22.9899 20.1202 23.5671 19.6395C24.1443 19.1588 24.1443 18.3732 23.5671 17.8925L17.5981 12.8978ZM9.15145 12.8978C5.64608 12.8978 2.81644 10.5412 2.81644 7.62173C2.81644 4.70231 5.64608 2.34566 9.15145 2.34566C12.6568 2.34566 15.4865 4.70231 15.4865 7.62173C15.4865 10.5412 12.6568 12.8978 9.15145 12.8978Z" fill="black"/>
+</svg>
+
     </form>
     <a class="nav-login-button" href="login.php">Login</a>
     <a class="nav-signup-button" href="sign-up.php">Sign Up</a>
