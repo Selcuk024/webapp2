13eeb0a4a87cea7e57e282c5a4ba1622492a9dff
DylanBackus <1202532@student.roc-nijmegen.nl>
2023-05-17 11:53:03 +0200

    nav topics toe gevoegd

--- website/navbar-not-logged-in.php
+++ website/navbar-not-logged-in.php
@@ -1,8 +1,11 @@
 <div class="nav-container">
     <img src="media/booking-logo.png" class="nav-logo">
-    <h1 class="nav-text" >World Wide Booking</h1>
-
-
+    <h1 class="nav-text">World Wide Booking</h1>
+    <div class="nav-topics-container">
+        <a href="my-account.php" class="my-account">My Account</a>
+        <a href="about-us.php" class="about-us">About Us</a>
+        <a href="contact.php" class="contact">Contact<a>
+    </div>
     <a class="nav-login-button" href="login.php">Register</a>
     <a class="nav-signup-button" href="sign-up.php">Sign In</a>
 </div>
