4a6fc3c002757e8dfbab40b2b9924d1f1309de68
DylanBackus <1202532@student.roc-nijmegen.nl>
2023-05-17 10:16:20 +0200

    navbar gemaakt

--- website/navbar-not-logged-in.php
+++ website/navbar-not-logged-in.php
@@ -5,7 +5,6 @@
     <h1 class="nav-text" >World Wide Booking</h1>
     <form class="searchbar"  method="post">
     <input type="text" class="search" name="zoekveld" placeholder="Search">
-    
     </form>
     <a class="nav-login-button" href="login.php">Login</a>
     <a class="nav-signup-button" href="sign-up.php">Sign Up</a>
